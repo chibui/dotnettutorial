e0987ebf6b86ade490fd9d3e8c6bc6edf8fad49f
Chi Bui <chi.bui@tal.com.au>
2020-11-04 09:13:13 +1100

    Learning Tuples

---
+++ LearningTupple/LearningTupple/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningTupple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Tuple<int, String, bool> myTuple = new Tuple<int, string, bool>(1, "Hello", true);
+
+            var myTuple = Tuple.Create(1, "Hello", true);
+
+            Console.WriteLine(myTuple.Item2);
+        }
+    }
+}
