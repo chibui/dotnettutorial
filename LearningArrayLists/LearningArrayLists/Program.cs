3af0401767bf59841173acb9db1bb2a8a128a9a0
Chi Bui <chi.bui@tal.com.au>
2020-11-04 08:07:08 +1100

    learning: - lists - array lists - bit arrays - dictionaries - concurrent dictionaries - hashtables

---
+++ LearningArrayLists/LearningArrayLists/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningArrayLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList List = new ArrayList();
+
+            List.Add("some string");
+
+            String s = (String)List[0];
+
+            Console.WriteLine(s.Length);
+        }
+    }
+}
