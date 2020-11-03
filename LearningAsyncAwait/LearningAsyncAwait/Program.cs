8b4f207d6c53e4e0eabec24590d5ddcc3e608e21
Chi Bui <chi.bui@tal.com.au>
2020-11-04 09:14:26 +1100

    Learning Async Await

---
+++ LearningAsyncAwait/LearningAsyncAwait/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningAsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Downloading File");
+            Download();
+            Console.ReadLine();
+        }
+
+        static async void Download()
+        {
+            //await Network.Download();
+
+            HttpClient client = new HttpClient();
+
+            var data = await client.GetStringAsync("https://google.com");
+
+            Console.WriteLine("Download Complete" + data);
+        }
+
+        // Mock external network library
+        //class Network
+        //{
+        //    static public Task Download()
+        //    {
+        //        return Task.Run(() => Thread.Sleep(3000) );
+        //    }
+        //}
+    }
+}
