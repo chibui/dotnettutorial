3f868e915eb1b082ac37be307143e338c7d722f5
Chi Bui <chi_bui@outlook.com>
2020-11-06 09:39:46 +1100

    learning Threads.

---
+++ LearningThreads/LearningThreads/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LearningThreads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Thread myThread = new Thread(new ThreadStart(Work));
+
+                myThread.Start();
+
+                Task.Run(() =>
+                {
+                    Console.WriteLine("starting task: " + Thread.CurrentThread.ManagedThreadId);
+
+                    Thread.Sleep(3000);
+
+                    Console.WriteLine("task complete");
+                });
+            }
+
+            Console.ReadLine();
+        }
+
+        static void Work()
+        {
+            Console.WriteLine("starting thread: " + Thread.CurrentThread.ManagedThreadId);
+
+            Thread.Sleep(3000);
+
+            Console.WriteLine("work complete");
+        }
+    }
+}
