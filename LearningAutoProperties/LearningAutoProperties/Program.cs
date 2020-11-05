fec4a84698c7664e04f0a23469b539e49ebc774f
Chi Bui <chi_bui@outlook.com>
2020-11-06 09:28:10 +1100

    Learning auto implemented properties.

---
+++ LearningAutoProperties/LearningAutoProperties/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningProperties
+{
+    class Player
+    {
+        public void Hit()
+        {
+            Random r = new Random();
+            Health -= r.Next(5, 50);
+        }
+
+        public int Health { get; private set; } = 100;
+
+        public bool IsAlive
+        {
+            get { return Health > 0; }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            for (int i = 0; i < 20; i++)
+            {
+                player.Hit();
+                Console.WriteLine("Is player alive: " + player.IsAlive);
+            }
+        }
+    }
+}
