f9f94fa09f64a613544f2d7d8d1e84d07bb6fd5e
Chi Bui <chi_bui@outlook.com>
2020-11-06 09:22:10 +1100

    Learning properties

---
+++ LearningProperties/LearningProperties/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningProperties
+{
+    class Player
+    {
+        int health = 100;
+
+        public void Hit()
+        {
+            Random r = new Random();
+            health -= r.Next(5, 50);
+        }
+
+        public bool IsAlive
+        {
+            get { return health > 0; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            for (int i = 0; i < 20; i++)
+            {
+                player.Hit();
+                Console.WriteLine("Is player alive: " + player.IsAlive);
+            }
+        }
+    }
+}
