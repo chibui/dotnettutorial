14993b73f15898f15ff6f653a0aeb53270b5fe0e
Chi Bui <chi_bui@outlook.com>
2020-11-23 11:05:12 +1100

    Learning string interpolation

---
+++ LearningStringInterpolation/LearningStringInterpolation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningStringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new { x = 1, y = 2 };
+
+            Console.WriteLine("x " + point.x + " y " + point.y); // string concatenation. garbage collector overhead
+
+            Console.WriteLine("x {0} y {1}", point.x, point.y); // string formatter. noticable overhead in bigger strings.
+
+            Console.WriteLine($"x {point.x} y {point.y}"); // string interpolation. 
+        }
+    }
+}
