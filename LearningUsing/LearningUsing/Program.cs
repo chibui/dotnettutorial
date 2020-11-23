0815f581200682630c7d9da587ef1d904636c450
Chi Bui <chi_bui@outlook.com>
2020-11-23 13:53:20 +1100

    Learning using

---
+++ LearningUsing/LearningUsing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningUsing
+{
+    class MyResource : IDisposable
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("Disposing our resource");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var resource = new MyResource())
+            {
+                Console.WriteLine("Using my resource");
+            }
+            Console.WriteLine("Finished");
+        }
+    }
+
+    // expect
+    // Using my resourc
+    // Disposing our resource // even though dispose is not explicitly called, it is called by the using statement.
+    // Finished
+}
