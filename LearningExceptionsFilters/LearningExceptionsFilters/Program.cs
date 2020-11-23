d6e4ff9a04ae98355c829378ba9c87ad6a1d644b
Chi Bui <chi_bui@outlook.com>
2020-11-23 13:44:43 +1100

    Learning exceptions filters

---
+++ LearningExceptionsFilters/LearningExceptionsFilters/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningExceptionFilters
+{
+    class PersonException : Exception
+    {
+        public PersonException(String name)
+        {
+            Name = name;
+        }
+        public String Name { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //throw new PersonException("Kim");
+                throw new PersonException("Bob");
+
+            }
+            catch (PersonException ex) when (ex.Name == "Kim")
+            {
+                Console.WriteLine("its from Kim, lets ignore it");
+            }
+            catch (PersonException ex) when (ex.Name == "Bob")
+            {
+                Console.WriteLine("its from Bob, lets do something");
+            }
+        }
+    }
+}
